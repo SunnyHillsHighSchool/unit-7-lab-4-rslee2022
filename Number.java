9f2d69c81336795cb0d1ef6567a164eac4a60725
Rachel Lee <600021256@fjuhsd.org>
2021-02-12 22:41:33 +0000

    Unit 7 Lab 4. Method to check if a number is odd. 2nd method to check if a number is perfect.

--- Number.java
+++ Number.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Rachel Lee
 
 
 //write the Number class
@@ -9,20 +9,74 @@
 //to guide you
 
 //header for Number class
+public class Number
+{
 
-//private instance variable (must be an Integer)
+  //private instance variable (must be an Integer)
+  private Integer num;
 
-//constructor
+  //constructors
+  public Number()
+  {
+    num = 0;
+  }
 
+  public Number(int n)
+  {
+    num = n;
+  }
 
-//getter method
+  //getter method
+  public Integer getNum()
+  {
+    return num;
+  }
 
+  //setter method
+  public void setNum(Integer a)
+  {
+    num = a;
+  }
+  
+  //isOdd method
+  public boolean isOdd()
+  {
+    //if the remainder of num divided by 2 equals 1 (using modulus: %)
+    if(num % 2 == 1)
+    {
+      //return true
+      return true;
+    }
+    //otherwise, return false
+    return false;
+  }
 
-//setter method
+  //isPerfect method
+  public boolean isPerfect()
+  {
+    //create integer sum, set to 0
+    int sum = 0;
+    //For loop: initialize loop control variable integer i to 1, condition - i is less than num, i increments by 1 each iteration
+    for(int i = 1; i < num; i++)
+    {
+      //if the remainder of the num divided by i is equal to 0
+      if(num % i == 0)
+        //add i to sum
+        sum += i;
+    }
+    //if sum is equal to num
+    if(sum == num)
+    {
+	    //return true
+      return true;
+    }
+    //otherwise, return false
+    return false;
+  }
 
-//isOdd method
-
-//isPerfect method
-
-
-//toString
+  //toString
+  public String toString()
+  {
+    return "" + num;
+  }
+}
